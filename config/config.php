1f96fe5f08cacdd682b17b0925ea790bd1c7ed22
Matt Vogt <mattjamesvogt@gmail.com>
2021-02-24 20:51:46 +1000

    dev

--- config/config.php
+++ config/config.php
@@ -8,5 +8,5 @@ return [
     'client_secret' => env('MYOB_CLIENT_SECRET', 'myob-client-secret'),
     'grant_type'    => env('MYOB_GRANT_TYPE', 'authorization_code'),
     'scope_type'    => env('MYOB_SCOPE_TYPE', 'CompanyFile'),
-    'redirect_uri'  => env('MYOB_REDIRECT_URI', '/login/myob')
+    'redirect_uri'  => 'https://nims.italicsbold.com.au/myob/login' //env('MYOB_REDIRECT_URI', '/login/myob')
 ];
